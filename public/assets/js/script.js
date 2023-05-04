a77ca033d86473e3b65f928787f6f55f2a92e28e
Muhammad Kashif zia <kashif.idenbrid@gmail.com>
2023-05-04 14:19:07 +0500

    small changes

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,1 +1,8 @@
-$((function(){$(".slick_area ul").slick({arrows:!1,dots:!0,autoplay:!0,autoplaySpeed:4e3})}));
+(function ($) {
+    $(".slick_area ul").slick({
+        arrows: !1,
+        dots: !0,
+        autoplay: !0,
+        autoplaySpeed: 4e3
+    })
+})(jQuery);
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -22,155 +22,26 @@
               <img src="/assets/images/index_slide_ttl_sp.png" alt="" class="sp">
             </div>
             <div class="slick_area">
-              <ul class="slick-initialized slick-slider slick-dotted">
-                <div class="slick-list draggable">
-                  <div class="slick-track"
-                    style="opacity: 1; width: 11000px; transform: translate3d(-4000px, 0px, 0px);">
-                    <div class="slick-slide slick-cloned" data-slick-index="-1" aria-hidden="true" tabindex="-1"
-                      style="width: 1000px;">
-                      <div>
-                        <li style="width: 100%; display: inline-block;">
-                          <a href="vr360/" class="ov" tabindex="-1">
-                            <img src="/assets/images/index_slide03_bg.jpg" alt="" class="pc">
-                            <img src="/assets/images/index_slide03_bg_sp.jpg" alt="" class="sp">
-                          </a>
-
-                        </li>
-                      </div>
-                    </div>
-                    <div class="slick-slide" data-slick-index="0" aria-hidden="true" role="tabpanel" id="slick-slide00"
-                      aria-describedby="slick-slide-control00" tabindex="-1" style="width: 1000px;">
-                      <div>
-                        <li style="width: 100%; display: inline-block;">
-                          <a href="vr360/" class="ov" tabindex="-1">
-                            <img src="/assets/images/index_slide07_bg.jpg" alt="" class="pc">
-                            <img src="/assets/images/index_slide07_bg_sp.jpg" alt="" class="sp">
-                          </a>
-
-                        </li>
-                      </div>
-                    </div>
-                    <div class="slick-slide" data-slick-index="1" aria-hidden="true" role="tabpanel" id="slick-slide01"
-                      aria-describedby="slick-slide-control01" tabindex="-1" style="width: 1000px;">
-                      <div>
-                        <li style="width: 100%; display: inline-block;">
-                          <a href="vr360/" class="ov" tabindex="-1">
-                            <img src="/assets/images/index_slide06_bg.jpg" alt="" class="pc">
-                            <img src="/assets/images/index_slide06_bg_sp.jpg" alt="" class="sp">
-                          </a>
-                        </li>
-                      </div>
-                    </div>
-                    <div class="slick-slide" data-slick-index="2" aria-hidden="true" role="tabpanel" id="slick-slide02"
-                      aria-describedby="slick-slide-control02" tabindex="-1" style="width: 1000px;">
-                      <div>
-                        <li style="width: 100%; display: inline-block;">
-                          <a href="vr360/" class="ov" tabindex="-1">
-                            <img src="/assets/images/index_slide01_bg.jpg" alt="" class="pc">
-                            <img src="/assets/images/index_slide01_bg_sp.jpg" alt="" class="sp">
-                          </a>
-
-                        </li>
-                      </div>
-                    </div>
-                    <div class="slick-slide slick-current slick-active" data-slick-index="3" aria-hidden="false"
-                      role="tabpanel" id="slick-slide03" aria-describedby="slick-slide-control03"
-                      style="width: 1000px;">
-                      <div>
-                        <li style="width: 100%; display: inline-block;">
-                          <a href="vr360/" class="ov" tabindex="0">
-                            <img src="/assets/images/index_slide02_bg.jpg" alt="" class="pc">
-                            <img src="/assets/images/index_slide02_bg_sp.jpg" alt="" class="sp">
-                          </a>
-                        </li>
-                      </div>
-                    </div>
-                    <div class="slick-slide" data-slick-index="4" aria-hidden="true" role="tabpanel" id="slick-slide04"
-                      aria-describedby="slick-slide-control04" tabindex="-1" style="width: 1000px;">
-                      <div>
-                        <li style="width: 100%; display: inline-block;">
-                          <a href="vr360/" class="ov" tabindex="-1">
-                            <img src="/assets/images/index_slide03_bg.jpg" alt="" class="pc">
-                            <img src="/assets/images/index_slide03_bg_sp.jpg" alt="" class="sp">
-                          </a>
-
-                        </li>
-                      </div>
-                    </div>
-                    <div class="slick-slide slick-cloned" data-slick-index="5" aria-hidden="true" tabindex="-1"
-                      style="width: 1000px;">
-                      <div>
-                        <li style="width: 100%; display: inline-block;">
-                          <a href="vr360/" class="ov" tabindex="-1">
-                            <img src="/assets/images/index_slide07_bg.jpg" alt="" class="pc">
-                            <img src="/assets/images/index_slide07_bg_sp.jpg" alt="" class="sp">
-                          </a>
-
-                        </li>
-                      </div>
-                    </div>
-                    <div class="slick-slide slick-cloned" data-slick-index="6" aria-hidden="true" tabindex="-1"
-                      style="width: 1000px;">
-                      <div>
-                        <li style="width: 100%; display: inline-block;">
-                          <a href="vr360/" class="ov" tabindex="-1">
-                            <img src="/assets/images/index_slide06_bg.jpg" alt="" class="pc">
-                            <img src="/assets/images/index_slide06_bg_sp.jpg" alt="" class="sp">
-                          </a>
-                        </li>
-                      </div>
-                    </div>
-                    <div class="slick-slide slick-cloned" data-slick-index="7" aria-hidden="true" tabindex="-1"
-                      style="width: 1000px;">
-                      <div>
-                        <li style="width: 100%; display: inline-block;">
-                          <a href="vr360/" class="ov" tabindex="-1">
-                            <img src="/assets/images/index_slide01_bg.jpg" alt="" class="pc">
-                            <img src="/assets/images/index_slide01_bg_sp.jpg" alt="" class="sp">
-                          </a>
-
-                        </li>
-                      </div>
-                    </div>
-                    <div class="slick-slide slick-cloned" data-slick-index="8" aria-hidden="true" tabindex="-1"
-                      style="width: 1000px;">
-                      <div>
-                        <li style="width: 100%; display: inline-block;">
-                          <a href="vr360/" class="ov" tabindex="-1">
-                            <img src="/assets/images/index_slide02_bg.jpg" alt="" class="pc">
-                            <img src="/assets/images/index_slide02_bg_sp.jpg" alt="" class="sp">
-                          </a>
-                        </li>
-                      </div>
-                    </div>
-                    <div class="slick-slide slick-cloned" data-slick-index="9" aria-hidden="true" tabindex="-1"
-                      style="width: 1000px;">
-                      <div>
-                        <li style="width: 100%; display: inline-block;">
-                          <a href="vr360/" class="ov" tabindex="-1">
-                            <img src="/assets/images/index_slide03_bg.jpg" alt="" class="pc">
-                            <img src="/assets/images/index_slide03_bg_sp.jpg" alt="" class="sp">
-                          </a>
-
-                        </li>
-                      </div>
-                    </div>
+              <div class="swiper mySwiper">
+                <div class="swiper-wrapper">
+                  <div class="swiper-slide"> 
+                    <img src="/assets/images/index_slide07_bg.jpg" alt class="pc">
+                    <img src="/assets/images/index_slide07_bg_sp.jpg" alt class="sp">
                   </div>
+                  <div class="swiper-slide"> 
+                    <img src="/assets/images/index_slide06_bg.jpg" alt class="pc">
+                    <img src="/assets/images/index_slide06_bg_sp.jpg" alt class="sp">
+                  </div>
+                  <div class="swiper-slide"> 
+                    <img src="/assets/images/index_slide02_bg.jpg" alt class="pc">
+                     <img src="/assets/images/index_slide02_bg_sp.jpg" alt class="sp"></div>
+                  <div class="swiper-slide"> 
+                    <img src="/assets/images/index_slide03_bg.jpg" alt class="pc">
+                    <img src="/assets/images/index_slide03_bg_sp.jpg" alt class="sp"></div>
+              
                 </div>
-                <ul class="slick-dots" role="tablist" style="display: block;">
-                  <li class="" role="presentation"><button type="button" role="tab" id="slick-slide-control00"
-                      aria-controls="slick-slide00" aria-label="1 of 5" tabindex="-1">1</button></li>
-                  <li role="presentation" class=""><button type="button" role="tab" id="slick-slide-control01"
-                      aria-controls="slick-slide01" aria-label="2 of 5" tabindex="-1">2</button></li>
-                  <li role="presentation" class=""><button type="button" role="tab" id="slick-slide-control02"
-                      aria-controls="slick-slide02" aria-label="3 of 5" tabindex="-1">3</button></li>
-                  <li role="presentation" class="slick-active"><button type="button" role="tab"
-                      id="slick-slide-control03" aria-controls="slick-slide03" aria-label="4 of 5" tabindex="0"
-                      aria-selected="true">4</button></li>
-                  <li role="presentation" class=""><button type="button" role="tab" id="slick-slide-control04"
-                      aria-controls="slick-slide04" aria-label="5 of 5" tabindex="-1">5</button></li>
-                </ul>
-              </ul>
+                <div class="swiper-pagination"></div>
+              </div>
             </div>
           </div>
         </div>
@@ -236,8 +107,14 @@
 </template>
 
 <script>
-  export default {
+export default{
+mounted(){
+  var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+}
+}
 
-  }
 </script>
-
